b5583cef1b06a6488acdea89320fa41feb109716
luisito <luisitomcp123@gmail.com>
2026-05-12 18:08:34 -0700

    sync: 2026-05-12 18:08

--- home/.local/bin/kbd-backlight-status.sh
+++ home/.local/bin/kbd-backlight-status.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 CURRENT=$(brightnessctl --device "asus::kbd_backlight" get)
 if [ "$CURRENT" -eq 0 ]; then
-    printf '{"text":" off","tooltip":"Keyboard backlight off — click to cycle brightness","class":"off"}\n'
+    printf '{"text":"󰌶","tooltip":"Keyboard backlight off — click to cycle brightness","class":"off"}\n'
+elif [ "$CURRENT" -eq 1 ]; then
+    printf '{"text":"󱩎","tooltip":"Keyboard backlight: 1/3 — click to cycle","class":"on"}\n'
+elif [ "$CURRENT" -eq 2 ]; then
+    printf '{"text":"󱩏","tooltip":"Keyboard backlight: 2/3 — click to cycle","class":"on"}\n'
 else
-    printf '{"text":" %s/3","tooltip":"Keyboard backlight: %s/3 — click to cycle","class":"on"}\n' "$CURRENT" "$CURRENT"
+    printf '{"text":"󱩐","tooltip":"Keyboard backlight: 3/3 — click to cycle","class":"on"}\n'
 fi
