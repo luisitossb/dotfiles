83d9a05adec4128b0abe2858964ed89f730a048d
luisito <luisitomcp123@gmail.com>
2026-05-15 22:18:20 -0700

    sync: 2026-05-15 22:18

--- home/.local/bin/toggle-kbd-backlight.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DEVICE="asus::kbd_backlight"
-CURRENT=$(brightnessctl --device "$DEVICE" get)
-MAX=$(brightnessctl --device "$DEVICE" max)
-NEXT=$(( (CURRENT + 1) % (MAX + 1) ))
-brightnessctl --device "$DEVICE" set "$NEXT"
